e9c6b383ff290c91a7983616a27673bd27017382
Efrain May <efrain_may_75@hotmail.com>
2021-07-13 17:11:54 -0500

    sliver app bar

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -4,10 +4,42 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    //cambiar luego con una instancia de movie
+    final String movie = ModalRoute.of(context).settings.arguments.toString() ?? 'no-movie';
+
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text("Details Screen"),
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppBar()
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: Text('movie.title', style: TextStyle(fontSize: 16)),
+        ),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'), 
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
         ),
       ),
     );
